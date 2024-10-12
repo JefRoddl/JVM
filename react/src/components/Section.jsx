1f82d4cbb8691fe540ba46ed59398402a5f7e96a
Jeferson Rodrigues <jefersonroddl@gmail.com>
2024-10-11 21:54:07 -0300

    adicionando segunda img e adicionando efeitos

--- react/src/components/Section.jsx
+++ react/src/components/Section.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Section = () => {
+
+  const [backGroundImage, setBackGroundImage] = useState(false)
+
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleChangeBackground = () => {
+    // Primeiro, esconde a imagem atual (fade out)
+    setIsVisible(false);
+
+    // Depois de 1.5s (tempo da transição), troca a imagem e faz o fade in
+    setTimeout(() => {
+      setBackGroundImage(!backGroundImage); // Alterna entre as imagens
+      setIsVisible(true); // Reaparece com a nova imagem
+    }, 1500); // Tempo da transição (1.5 segundos)
+  };
+
   return (
-    <div className='divSection'>
-        <h1 className='FraseSection1'>SEMPRE A SUA DISPOSIÇÃO</h1>
-        <button className='buttonSeta'><img className='imgIcone' src='../src/assets/images/seta-direita.png' /></button>
+    <div className={`${backGroundImage ? 'divSection2' : 'divSection'} ${!isVisible ? 'hidden' : ''}`}>
+      <h1 className='FraseSection1'>SEMPRE A SUA DISPOSIÇÃO</h1>
+      <button className='buttonSeta' onClick={handleChangeBackground}>
+        <img className='imgIcone' src='../src/assets/images/seta-direita.png' alt='Seta' />
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default Section
