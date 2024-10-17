ec2d1ca4467ed874ca8c1efcfb5f939d81d4edf3
Jeferson Rodrigues <jefersonroddl@gmail.com>
2024-10-16 22:00:53 -0300

    finalizando section servicos

--- react/src/components/OurServices.jsx
+++ react/src/components/OurServices.jsx
@@ -8,6 +8,14 @@ const OurServices = () => {
             <img className='iconeDescarga' src="../src/assets/images/iconeDescarga.png" alt="" />
             <p className='textServiceDescarga'>Carga e descarga rápida e segura, realizada por profissionais especializados para garantir eficiência no processo</p>
         </section>
+        <section className='conferente'>
+            <p className='textServiceConferente'>Oferecemos mão de obra especializada para o serviço de conferência, garantindo precisão e controle rigoroso de mercadorias</p>
+            <img className='iconeConferente' src="../src/assets/images/iconeConferente.png" alt="" />
+        </section>
+        <section className='limpeza'>
+            <img className='iconeDescarga' src="../src/assets/images/iconeLimpeza.png" alt="" />
+            <p className='textServiceDescarga'>serviços de limpeza empresarial com equipe treinada e dedicada, garantindo ambientes sempre limpos e organizados</p>
+        </section>
     </div>
   )
 }
