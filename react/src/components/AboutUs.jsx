d9f47711ff1c7dc7bd2f22fb7c992acaa73994d2
Jeferson Rodrigues <jefersonroddl@gmail.com>
2024-10-17 23:50:25 -0300

    adicionando  missao ao sobra nos

--- react/src/components/AboutUs.jsx
+++ react/src/components/AboutUs.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import imagemFundo4 from "/src/assets/images/imagemFundo4.png"
+import alvo from "/src/assets/images/alvo.png"
 
 const AboutUs = () => {
   return (
     <section className='containerAboutUs'>
-        <img src={imagemFundo4} alt="" className='imgFundoAbout'/>
+        <h1 className='h1About'>Sobre a JVM</h1>
+        <section className='sectionMission'>
+            <img src={alvo} alt="" className='alvo'/>
+            <h1>MISSÃO</h1>
+        </section>
+        <p>A missão da JVM é fornecer serviços de mão de obra especializada com excelência, garantindo agilidade, segurança e eficiência em cada operação</p>
+        
     </section>
   )
 }
