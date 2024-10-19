21035f02133ab650477c709717735bfd11eb2917
Jeferson Rodrigues <jefersonroddl@gmail.com>
2024-10-19 19:57:30 -0300

    adicionando API intersection e efeitos ao section mission

--- react/src/components/AboutUs.jsx
+++ react/src/components/AboutUs.jsx
@@ -1,18 +1,45 @@
-import React from 'react'
-import alvo from "/src/assets/images/alvo.png"
+import React, { useState, useEffect, useRef } from 'react';
+import alvo from "/src/assets/images/alvo.png";
 
 const AboutUs = () => {
-  return (
-    <section className='containerAboutUs'>
-        <h1 className='h1About'>Sobre a JVM</h1>
-        <section className='sectionMission'>
-            <img src={alvo} alt="" className='alvo'/>
-            <h1>MISSÃO</h1>
-        </section>
-        <p>A missão da JVM é fornecer serviços de mão de obra especializada com excelência, garantindo agilidade, segurança e eficiência em cada operação</p>
-        
-    </section>
-  )
-}
+  const [isVisible, setIsVisible] = useState(false);
+  const sectionRef = useRef(null);
 
-export default AboutUs
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0];
+        if (entry.isIntersecting) {
+          setIsVisible(true); // Define como visível quando o elemento está no viewport
+          observer.disconnect(); // Desconecta o observer após a primeira vez
+        }
+      },
+      { threshold: 0.1 } // Muda para 10% de visibilidade
+    );
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current); // Observa o elemento
+    }
+
+    return () => {
+      if (sectionRef.current) {
+        observer.unobserve(sectionRef.current); // Limpa a observação
+      }
+    };
+  }, []);
+
+  return (
+    <section ref={sectionRef} className='containerAboutUs'>
+      <h1 className='h1About'>Sobre a JVM</h1>
+      <section className={`sectionMission ${isVisible ? "slideIn" : ""}`}>
+        <img src={alvo} alt="" className='alvo' />
+        <h1>MISSÃO</h1>
+      </section>
+      <p className={`missionP ${isVisible ? "slideIn" : ""}`}>
+        A missão da JVM é fornecer serviços de mão de obra especializada com excelência, garantindo agilidade, segurança e eficiência em cada operação.
+      </p>
+    </section>
+  );
+};
+
+export default AboutUs;
