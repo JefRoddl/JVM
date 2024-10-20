fb96326b7ab2bec06923788e8aec868db8ae9868
Jeferson Rodrigues <jefersonroddl@gmail.com>
2024-10-20 13:56:51 -0300

    adicionando a section visao

--- react/src/components/AboutUs.jsx
+++ react/src/components/AboutUs.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import alvo from "/src/assets/images/alvo.png";
+import lampada from "/src/assets/images/lampada.png"
 
 const AboutUs = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -14,7 +15,7 @@ const AboutUs = () => {
           observer.disconnect(); // Desconecta o observer após a primeira vez
         }
       },
-      { threshold: 0.1 } // Muda para 10% de visibilidade
+      { threshold: 0.5 } // Muda para 10% de visibilidade
     );
 
     if (sectionRef.current) {
@@ -31,13 +32,26 @@ const AboutUs = () => {
   return (
     <section ref={sectionRef} className='containerAboutUs'>
       <h1 className='h1About'>Sobre a JVM</h1>
-      <section className={`sectionMission ${isVisible ? "slideIn" : ""}`}>
+
+
+      <section className={`sectionMission ${isVisible ? "slideUp" : ""}`}>
         <img src={alvo} alt="" className='alvo' />
         <h1>MISSÃO</h1>
       </section>
-      <p className={`missionP ${isVisible ? "slideIn" : ""}`}>
+      <p className={`missionP ${isVisible ? "slideUp" : ""}`}>
         A missão da JVM é fornecer serviços de mão de obra especializada com excelência, garantindo agilidade, segurança e eficiência em cada operação.
       </p>
+
+
+      <section className={`sectionVision ${isVisible ? "slideUp" : ""}`}>
+        <img src={lampada} alt="" className='lampada' />
+        <h1>VISÃO</h1>
+      </section>
+      <p className={`visionP ${isVisible ? "slideUp" : ""}`}>
+        A missão da JVM é fornecer serviços de mão de obra especializada com excelência, garantindo agilidade, segurança e eficiência em cada operação.
+      </p>
+
+
     </section>
   );
 };
