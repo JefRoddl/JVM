f4b89c1342157b6c7d294954ae3f1c65beaf5ff1
Jeferson Rodrigues <jefersonroddl@gmail.com>
2024-10-24 21:10:46 -0300

    alterando  imagem do portfolio

--- react/src/components/Portfolio.jsx
+++ react/src/components/Portfolio.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import imgPortfolio from "/src/assets/images/imgPortfolio.png";
+import imgPortfolio1 from "/src/assets/images/imgPortfolio1.png";
 
 const Portfolio = () => {
   return (
     <div id='portfolio' className='containerPortfolio'>
+      <img src={imgPortfolio1} alt="" className='imgPortfolio'/>
         <div className='acessPortfolio'>
-          <h2>Nosso trabalho em ação</h2>
-          <a href="" className='acessPortfolioh2'><h2>Acessar portfolio</h2></a>
+          <h2>NOSSO TRABALHO  EM AÇÃO</h2>
+          <a href="" className='acessPortfolioh2'><h2>ACESSAR PORTFOLIO</h2></a>
         </div>
     </div>
   )
