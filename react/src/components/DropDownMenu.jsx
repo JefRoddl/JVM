fccb330e0fc3df67c6d4e6b931a8fa2e5655e1bc
Jeferson Rodrigues <jefersonroddl@gmail.com>
2024-10-24 21:57:12 -0300

    ajustando offset do portfolio

--- react/src/components/DropDownMenu.jsx
+++ react/src/components/DropDownMenu.jsx
@@ -6,9 +6,9 @@ const DropDownMenu = () => {
   return (
     
     <div className='DropDownMenuContainer'>
-        <Link to="servicos" smooth={true} duration={500}>Nossos serviços</Link>
-      <Link to="portfolio" smooth={true} duration={500}>Portfólio</Link>
-      <Link to="sobre" smooth={true} duration={500}>Sobre Nós</Link>
+        <Link to="servicos" smooth={true} duration={500} offset={-window.innerHeight / 8.5}>Nossos serviços</Link>
+      <Link to="portfolio" smooth={true} duration={500} offset={-window.innerHeight / 5}>Portfólio</Link>
+      <Link to="sobre" smooth={true} duration={500} offset={-window.innerHeight / 9}>Sobre Nós</Link>
       <Link to='inicio'smooth={true} duration={500}>Inicio</Link>
       </div>
   )
